9f76c512088d2d931d1d6979d1f2bc3f05f59f24
bascule-aizu <aizu@bascule.co.jp>
2017-07-05 16:09:30 +0900

    added access_token

--- service/line/auth.py
+++ service/line/auth.py
@@ -4,6 +4,7 @@ from __future__ import division, print_function, absolute_import, unicode_litera
 import base64
 import hashlib
 import hmac
+import requests
 
 def signature(channel_secret, msg):
     hash = hmac.new(channel_secret.encode('utf-8'),
@@ -11,3 +12,12 @@ def signature(channel_secret, msg):
                     hashlib.sha256).digest()
     signature = base64.b64encode(hash)
     return signature
+
+def access_token(client_id, client_secret):
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    payload = {'grant_type': 'client_credentials',
+               'client_id': client_id,
+               'client_secret': client_secret}
+    res = requests.post(url, headers=headers, data=payload)
+    if res.raise_for_status() is None:
+        return res.json()
