190586ff6925aa7817822abcebd64283705c73de
Tsuyoshi Aizu <aizu@bascule.co.jp>
2018-02-22 22:42:44 +0900

    fixed ec2.py

--- ojosjp/service/aws/ec2.py
+++ ojosjp/service/aws/ec2.py
@@ -56,10 +56,10 @@ class InstanceMetadata(object):
         logger.info('START instance_id')
 
         if self._instance_id is None:
-            if self._mock:
-                self._instance_id = self._kwargs.get('instance_id', self.DEFAULT_INSTANCE_ID)
-            else:
+            try:
                 self._instance_id = self.get_metadata('instance-id')
+            except:
+                self._instance_id = self._kwargs.get('instance_id', self.DEFAULT_INSTANCE_ID)
 
         logger.info('RETURN %s', self._instance_id)
         logger.info('END instance_id')
@@ -70,10 +70,10 @@ class InstanceMetadata(object):
         logger.info('START public_ip')
 
         if self._public_ip is None:
-            if self._mock:
-                self._public_ip = self._kwargs.get('public_ip', self.DEFAULT_PUBLIC_IP)
-            else:
+            try:
                 self._public_ip = self.get_metadata('public-ipv4')
+            except:
+                self._public_ip = self._kwargs.get('public_ip', self.DEFAULT_PUBLIC_IP)
 
         logger.info('RETURN %s', self._public_ip)
         logger.info('END public_ip')
@@ -84,10 +84,10 @@ class InstanceMetadata(object):
         logger.info('START local_ip')
 
         if self._local_ip is None:
-            if self._mock:
-                self._local_ip = self._kwargs.get('local_ip', self.DEFAULT_LOCAL_IP)
-            else:
+            try:
                 self._local_ip = self.get_metadata('local-ipv4')
+            except:
+                self._local_ip = self._kwargs.get('local_ip', self.DEFAULT_LOCAL_IP)
 
         logger.info('RETURN %s', self._local_ip)
         logger.info('END local_ip')
@@ -98,10 +98,10 @@ class InstanceMetadata(object):
         logger.info('START tags')
 
         if self._tags is None:
-            if self._mock:
-                self._tags = self._kwargs.get('tags', self.DEFAULT_TAGS)
-            else:
+            try:
                 self._tags = self._get_tags(self.instance_id)
+            except:
+                self._tags = self._kwargs.get('tags', self.DEFAULT_TAGS)
 
         logger.info('RETURN %s', self._tags)
         logger.info('END tags')
@@ -137,12 +137,12 @@ class InstanceMetadata(object):
         logger.info('END roles')
         return roles
 
-    def __init__(self, client=None, mock=False, **kwargs):
+    def __init__(self, client=None, max_tries=0, **kwargs):
         logger.info('START __init__')
         logger.info('INPUT client=%s, mock=%s, kwargs=%s', client, mock, kwargs)
 
         self._client = client if isinstance(client, BaseClient) else get_client('ec2')
-        self._mock = mock
+        self._max_tries = max_tries
         self._kwargs = kwargs
 
         logger.info('END __init__')
@@ -151,11 +151,17 @@ class InstanceMetadata(object):
         logger.info('START get_metadata')
         logger.info('INPUT category=%s', category)
 
-        try:
-            res = requests.get(self.EC2_META_DATA_URL % category)
-        except Exception:
-            time.sleep(1)
-            res = self.get_metadata(category)
+        timeout = 1
+        tries = 0
+        while:
+            try:
+                res = requests.get(self.EC2_META_DATA_URL % category,
+                                   timeout=timeout)
+            except Exception as e:
+                timeout *= 2
+                tries += 1
+                if self._max_tries != 0 and tries >= self._max_tries:
+                    raise e
 
         logger.info('SET res=%s', '{}'.format(res.__dict__))
         logger.info('RETURN %s', res.text)
@@ -163,14 +169,21 @@ class InstanceMetadata(object):
         return res.text
 
     def _get_tags(self, instance_id):
-        try:
-            filters = [{'Name': 'instance-id', 'Values': [instance_id]}]
-            res = self._client.describe_instances(Filters=filters)
-            logger.info('SET res=%s', '{}'.format(res))
-            tags = {t['Key']: t['Value']
-                    for t in res['Reservations'][0]['Instances'][0]['Tags']}
-        except Exception:
-            time.sleep(1)
-            tags = self._get_tags(instance_id)
+        filters = [{'Name': 'instance-id', 'Values': [instance_id]}]
+        timeout = 1
+        tries = 0
+        while:
+            try:
+                res = self._client.describe_instances(Filters=filters)
+                logger.info('SET res=%s', '{}'.format(res))
+            except Exception as e:
+                time.sleep(timeout)
+                timeout *= 2
+                tries += 1
+                if self._max_tries != 0 and tries >= self._max_tries:
+                    raise e
+
+        tags = {t['Key']: t['Value']
+                for t in res['Reservations'][0]['Instances'][0]['Tags']}
 
         return tags
